3214f951fa9d02fe72810deb7679477698b386c3
geedtd <grcazares@gmail.com>
2023-04-26 22:58:25 -0700

    displaying marker properly and adding a marker to show on map

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-
 import './App.css'
-
-import { MapContainer } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
 function App() {
+
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Hello, I am pop 1"
+    }
+  ]
   
 
   return (
     <>
-      <MapContainer center={[48.8566, 2.3522]} zoom={1} >
+      <MapContainer 
+        center={[48.8566, 2.3522]} 
+        zoom={13}
+        
+      >
+        <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
 
+        {markers.map(marker => (
+          <Marker position={marker.geocode}/>
+        ))}
       </MapContainer>
     </>
   )
